09f6312504086e56c31ce5e96db2af6e8de98f05
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-30 13:12:57 +0300

    wire the views to urls

--- notiflow_backend/urls.py
+++ notiflow_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from notifications.views import HealthView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notifications/',  include('notifications.urls')),
+    path('health/',        HealthView.as_view(), name='health'),
 ]
